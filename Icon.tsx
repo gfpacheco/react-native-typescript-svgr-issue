919eb5a2ee9c5ef79eaa6ac5fb110c59eeae0734
Guilherme Pacheco <guilherme.f.pacheco@gmail.com>
2020-08-25 15:35:39 -0300

    Add icon

---
+++ Icon.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Svg, {Path} from 'react-native-svg';
+
+function SvgComponent(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <Svg height={24} viewBox="0 0 24 24" width={24} {...props}>
+      <Path d="M0 0h24v24H0z" fill="none" />
+      <Path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+    </Svg>
+  );
+}
+
+export default SvgComponent;
